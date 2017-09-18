6defecacf79baf3f7414550321c15b0e1e3f2606
Dallin Parker <dallin.r.parker@gmail.com>
2017-09-17 22:25:27 -0400

    added in a simple example of code splitting

--- learnWebpack/js_modules/src/index.js
+++ learnWebpack/js_modules/src/index.js
@@ -1,5 +1,9 @@
-import sum from './sum'
-import './image_viewer'
+const button = document.createElement('button')
+button.innerText = 'Click Me'
+button.onclick = () => {
+	System.import('./image_viewer').then(module => {
+		module.default()
+	})
+}
 
-const total = sum(10,5)
-console.log('total: ', total)
+document.body.appendChild(button)
